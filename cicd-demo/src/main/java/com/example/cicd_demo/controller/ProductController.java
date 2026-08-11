09783d8071e063ec2c2f5855fa1bae926c46a0dd
Divya Sagar <divya_sagar@epam.com>
2026-08-11 10:28:36 +0530

    adding demo project

---
+++ cicd-demo/src/main/java/com/example/cicd_demo/controller/ProductController.java
@@ -0,0 +1,14 @@
+package com.example.cicd_demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ProductController {
+    @GetMapping
+    public String test(){
+        return "This is working";
+    }
+}
